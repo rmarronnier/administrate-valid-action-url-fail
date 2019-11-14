d149cb3396adab34ac1e35276e847d8fdcf15e5e
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-14 16:41:23 +0100

    Minimum administrate app with valid_action custom method

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :admin do
+      resources :pets
+
+      root to: "pets#index"
+    end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
